707faac1c92630daffd584d26b509d31ad596583
JonJenson <132286169+JonJenson@users.noreply.github.com>
2023-12-13 12:22:35 +0530

    Third commit

--- project_management/settings.py
+++ project_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-sry+gn(j+gcd^2^mfz^@bd91!y&h%t)%=)@u$65a3@16k54lpa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
